b93ed16ff2082a82f743df41b61db051fe7a92b1
wile <williamle@hotmail.dk>
2025-10-28 21:37:37 +0100

    Update server.go

--- server/server.go
+++ server/server.go
@@ -40,6 +40,24 @@ func (s *ChitChatServer) Subscribe(req *proto.SubscribeRequest, stream proto.Chi
 	s.subscribers[clientID] = stream
 	s.timestamp++
 	currentTime := s.timestamp
+
+	// Create and send JOIN broadcast to all other clients
+	joinBroadcast := &proto.BroadCast{
+		Type:      proto.BroadCast_JOIN,
+		ClientId:  clientID,
+		Timestamp: currentTime,
+		Message:   "",
+	}
+
+	// Send JOIN message to all existing clients (except the new one)
+	for id, subscriber := range s.subscribers {
+		if id != clientID { // Don't send to the joining client
+			if err := subscriber.Send(joinBroadcast); err != nil {
+				log.Printf("Failed to send JOIN broadcast to %s: %v", id, err)
+			}
+		}
+	}
+
 	s.mutex.Unlock()
 
 	log.Printf("Participant %s joined Chit Chat at logical time %d", clientID, currentTime)
